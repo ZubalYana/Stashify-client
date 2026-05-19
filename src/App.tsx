66c9ec0c7bbfd4a66a4678443265d6962daaaee6
Yana <zubalana0@gmail.com>
2026-05-19 14:43:55 +0300

    set up tailwind and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='w-screen'>
+    <Routes>
+      <Route path='/' element={<div className='w-full'>Root element of Stashify!</div>}/>
+    </Routes>
+    </div>
   )
 }
 
