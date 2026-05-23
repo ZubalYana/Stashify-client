ffa6ee9bf35db20017eb1ed8da583c6a7c1967c7
Yana <zubalana0@gmail.com>
2026-05-23 05:31:06 +0300

    create primary button, style & animate it, create a layout of the snippet creation popup

---
+++ src/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,32 @@
+import type { LucideIcon } from "lucide-react"
+
+interface PrimaryButtonProps {
+  Icon: LucideIcon;
+  text: string;
+  onClick: () => void;
+}
+
+export default function PrimaryButton({ Icon, text, onClick }: PrimaryButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        group
+        px-6 py-3 rounded-[16px] bg-[#F07020]
+        flex items-center gap-x-2 cursor-pointer
+        hover:bg-[#FF8533]
+        hover:shadow-[0_0_0_6px_rgba(240,112,32,0.15)]
+        active:scale-95
+        transition-all duration-200 ease-out
+      "
+    >
+      <Icon
+        className="transition-transform duration-200 group-hover:scale-110"
+        strokeWidth={2.5}
+      />
+      <span className="font-bold text-[24px] uppercase transition-transform duration-200 group-hover:translate-x-0.5">
+        {text}
+      </span>
+    </button>
+  )
+}
--- src/components/popups/SnippetCreation.tsx
+++ src/components/popups/SnippetCreation.tsx
@@ -1,5 +1,7 @@
 export default function SnippetCreation(){
     return(
-        <div></div>
+        <div className="w-full lg:w-[80%] min-h-0 bg-[#121212]">
+            <h2>Create a new snippet</h2>
+        </div>
     )
 }
